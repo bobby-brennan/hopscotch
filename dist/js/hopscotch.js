6a84191a07c0ee8708e6bae2a5f70d06c11b6659
Bobby Brennan <bobby.brennan@gmail.com>
2017-01-26 20:01:01 +0000

    build dist/

--- dist/js/hopscotch.js
+++ dist/js/hopscotch.js
@@ -848,8 +848,8 @@
       }
 
       // Find arrow among new child elements.
-      children = el.children;
-      numChildren = children.length;
+      var children = el.children;
+      var numChildren = children.length;
       for (i = 0; i < numChildren; i++){
         node = children[i];
 
